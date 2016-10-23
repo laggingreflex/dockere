820778d5e3962c8ceb6e1ddbfabfa1321fe8dc75
root <root>
2016-10-23 07:59:19 -0700

    Mount homedir as /root

--- src/utils/docker.js
+++ src/utils/docker.js
@@ -1,6 +1,6 @@
 import path from 'path';
 import { exec, splitCommandStr } from './child-process';
-import { cwdFull, cwdBase } from './fs';
+import { cwdFull, cwdBase, homedir } from './fs';
 
 export function build() {
   return exec( ...splitCommandStr( `docker build -t ${cwdBase} .` ) );
@@ -9,6 +9,7 @@ export function run( command ) {
   return exec( 'docker', [
     'run', '-it', '--rm',
     '--volume', `${cwdFull}${path.sep}:/${cwdBase}`,
+    '--volume', `${homedir}${path.sep}:/root`,
     cwdBase, ...command
     // 'bash', '-c', `cd /${cwdBase} && ${command || 'bash -li'}`
   ] );
